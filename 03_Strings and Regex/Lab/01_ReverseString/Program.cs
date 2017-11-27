fb8573a54917c93b959bf46d9c650b3230ff4d08
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-11-27 23:32:13 +0200

    finished lab exercises 1

---
+++ 03_Strings and Regex/Lab/01_ReverseString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ReverseString
+{
+    static void Main(string[] args)
+    {
+        //string s = Console.ReadLine();
+
+        //char[] charArray = s.ToCharArray();
+
+        //var reversedString = string.Empty;
+
+        //for (int i = s.Length - 1; i >= 0; i--)
+        //{
+        //    reversedString += charArray[i];
+        //}
+
+        //Console.WriteLine(string.Join(string.Empty, reversedString));
+
+        string s = Console.ReadLine();
+
+        Console.WriteLine(Reverse(s));
+    }
+
+    public static string Reverse(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
+
