9de657e609f3b3b62eb88bb176b55db483879095
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-12-05 19:19:02 +0200

    finised Regex and string lab

--- 03_Strings and Regex/Lab/09_MatchNumbers/Program.cs
+++ 03_Strings and Regex/Lab/09_MatchNumbers/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -10,7 +11,17 @@ class MatchNumbers
 {
     static void Main(string[] args)
     {
+        string pattern = @"(^|(?<=\s))-?(?:\d+)?(?:\.\d+)?(?=\s|$)";
+        string input = Console.ReadLine();
 
+        RegexOptions options = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            Console.Write(m.Value + " ");
+        }
+
+        Console.WriteLine();
     }
 }
 
---
+++ 03_Strings and Regex/Lab/10_ReplaceATag/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class ReplaceATag
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            string inputLine = Console.ReadLine();
+            if (inputLine == "end")
+                break;
+
+            string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+            string replacement = @"[URL href=$1]$2[/URL]";
+            string replaced = Regex.Replace(inputLine, pattern, replacement);
+            Console.WriteLine(replaced);
+        }
+    }
+}
+
