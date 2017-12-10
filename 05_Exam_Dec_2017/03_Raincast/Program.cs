60cfcdc7e56a14bfb3072e399726d39d46b6a7c6
Kostadin Belchev <kostadin.belchev@gmail.com>
2017-12-10 16:41:55 +0200

    part 2 of exam December

---
+++ 05_Exam_Dec_2017/03_Raincast/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+class Raincast
+{
+    static void Main(string[] args)
+    {
+        var typeFound = false;
+        var sourceFound = false;
+        var forecastFound = false;
+
+        var type = string.Empty;
+        var source = string.Empty;
+        var forecast = string.Empty;
+
+        while (true)
+        {
+            string inputLine = Console.ReadLine();
+
+            if (inputLine == "Davai Emo")
+                break;
+            RegexOptions options = RegexOptions.Multiline;
+            string pattern1 = @"^(Type: )(Normal|Warning|Danger)$";
+            var matches1 = Regex.Matches(inputLine, pattern1, options);
+            string pattern2 = @"^(Source: )(\w+)$";
+            var matches2 = Regex.Matches(inputLine, pattern2, options);
+            string pattern3 = @"^(Forecast: )([^.,!?\n]*)$";
+            var matches3 = Regex.Matches(inputLine, pattern3, options);
+
+            if (!typeFound && matches1.Count > 0)
+                foreach (Match m in matches1)
+                {
+                    type = m.Groups[2].Value;
+                    typeFound = true;
+                }
+            if (!sourceFound && typeFound && matches2.Count > 0)
+                foreach (Match m in matches2)
+                {
+                    source = m.Groups[2].Value;
+                    sourceFound = true;
+                }
+            if (!forecastFound && sourceFound && matches3.Count > 0)
+                foreach (Match m in matches3)
+                {
+                    forecast = m.Groups[2].Value;
+                    forecastFound = true;
+                }
+            if (typeFound && sourceFound && forecastFound)
+            {
+                Console.WriteLine($"({type}) {forecast} ~ {source}");
+                typeFound = false;
+                sourceFound = false;
+                forecastFound = false;
+            }
+        }
+    }
+}
+
---
+++ 05_Exam_Dec_2017/04_RainAir/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class RainAir
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, List<int>> customerFlights = new Dictionary<string, List<int>>();
+
+        while (true)
+        {
+            string inputLine = Console.ReadLine();
+            if (inputLine == "I believe I can fly!")
+            {
+                break;
+            }
+            string[] inputLineTokens = inputLine.Split("= ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<int> customerFlightList = new List<int>();
+            string customerName = string.Empty;
+
+            if (inputLineTokens.Length == 2)
+            {
+                customerName = inputLineTokens[0];
+                var customerFlightNumber = 0;
+                try
+                {
+                    customerFlightNumber = int.Parse(inputLineTokens[1]);
+                    customerFlightList.Add(customerFlightNumber);
+                }
+                catch
+                {
+                    string customer2Name = inputLineTokens[1];
+                    customerFlights[customerName] = customerFlights[customer2Name];
+                }
+            }
+            else
+            {
+                customerName = inputLineTokens[0];
+                var listToAdd = inputLineTokens.Skip(1).Select(int.Parse).ToList();
+                customerFlightList.AddRange(listToAdd);
+            }
+
+            if (!customerFlights.ContainsKey(customerName)) //if customerName does not exist we add it with his flights
+            {
+                customerFlights[customerName] = customerFlightList;
+                //ERROR IS HERE, you have to foreach the list and fill it manually!!! Lists are ference type of data
+            }
+            else
+            {
+                customerFlights[customerName].AddRange(customerFlightList);
+            }
+            
+        }
+        //output
+        //ordering
+        foreach (var item in customerFlights.Values)
+        {
+            item.Sort();
+        }
+        customerFlights = customerFlights.OrderByDescending(x => x.Value.Count).ThenBy(x => x.Key).ToDictionary(x => x.Key, y => y.Value);
+        //printing
+        foreach (var keyValuePair in customerFlights)
+        {
+            Console.WriteLine($"#{keyValuePair.Key} ::: {string.Join(", ", keyValuePair.Value)}");
+        }
+    }
+}
+
